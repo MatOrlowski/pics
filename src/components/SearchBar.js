b7070835a48e13b0b92c7806bd6539098e8ec539
Mateusz Orłowski <mateusz0799@gmail.com>
2021-07-20 19:04:05 +0200

    Created searchbar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" }; //Initializing state
+
+  onFormSubmit(event) {
+    event.preventDefault(); //preventing from self-refreshing
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term} //go look at state to get current value
+              onChange={(e) => this.setState({ term: e.target.value })} //inform state what user typed
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
